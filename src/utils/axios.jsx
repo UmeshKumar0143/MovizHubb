4b8e34b718648c54fe3af2e3b70239c93aca0b44
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-08-31 10:59:08 +0530

    fixed issue

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL:"https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDc1MDQzMjFkZGU5MzJiMDgxZWNjZWQ3ODU5YTllNiIsIm5iZiI6MTcyNDM0OTcwOC41MTU0NzcsInN1YiI6IjY2Yzc3Yjg1NTEyNWFhOTliOGQ3ZjIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5jSsDqEHC2lT70v_7UIQEdFoWuPzp9Ax2eD3l6ddxzg'
+        Authorization: `Bearer ${process.env.API_KEY} `
       }
 }
 )
